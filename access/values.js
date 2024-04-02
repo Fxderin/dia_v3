e1e0a9637a946d98bdcbff4ef1d2a2ac386d25ff
Fxderin <148588010+Fxderin@users.noreply.github.com>
2024-04-03 00:32:38 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,15 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "17.03.2006"; // Дата рождения
+var fullname = "Бондаренко Iлля Антонович" // ФИО
+var name = "Iлля" // Имя
+
+// Паспорт
+var passport_id = "008759371" // Номер паспорта
+
+// КПП
+var kpp_id = "3805401387" // ИНН
+
